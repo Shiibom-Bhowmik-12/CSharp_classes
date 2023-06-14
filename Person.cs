7c641d6c7512bd6536ec51861e48a227064d0869
Shibom Kumar Bhowmik <skrbhowmik.2023@gmail.com>
2023-06-14 22:05:19 +0530

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace non_primitive
+{
+    public class Person
+    {
+        public string FirstName="";
+        public string LastName="";
+
+        public void Introduce()
+        {
+            Console.WriteLine("My Name is " + FirstName + " " + LastName);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using non_primitive.Math;
+
+namespace non_primitive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1=new Person();
+            p1.FirstName = "Shibom";
+            p1.LastName = "Bhowmik";
+            p1.Introduce();
+
+            //Math class is seperated in another folder because it differs bussiness logic.
+            //Whenever we are dealing with different logic we separate the classes in diffrent folders,
+            //in different namespaces
+            var calc = new Calculator();
+            Console.WriteLine(calc.Add(10,30));
+        }
+    }
+}
